1dad3c59dc96c4f282f0bed40ec247ae43380ee5
kblc9l <97379286+kblc9l@users.noreply.github.com>
2024-02-25 10:30:08 +0400

    Эспрессо

--- main.py
+++ main.py
@@ -1,46 +1,28 @@
-import random
 import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
+from PyQt5 import uic
+import sqlite3
 
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 
-
-class Example(QWidget):
+class Coffee(QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.initUI()
+        super(Coffee, self).__init__()
 
-    def initUI(self):
-        self.setGeometry(300, 300, 200, 200)
-        self.setWindowTitle('123')
-        self.btn = QPushButton('Draw', self)
-        self.btn.move(70, 150)
-        self.btn.resize(60, 40)
-        self.do_paint = False
-        self.btn.clicked.connect(self.paint)
+        uic.loadUi('design.ui', self)
+        self.open_table()
 
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_flag(qp)
-            qp.end()
-
-    def paint(self):
-        self.do_paint = True
-        self.repaint()
-
-    def draw_flag(self, qp):
-        a = random.randint(10, 100)
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        qp.setBrush(QColor(r, g, b))
-        qp.drawEllipse(random.randint(0, 200), random.randint(0, 200), a, a)
+    def open_table(self):
+        con = sqlite3.connect("coffee.sqlite")
+        cur = con.cursor()
+        data = cur.execute("SELECT * FROM coffe_info").fetchall()
+        self.tableWidget.setRowCount(len(data))
+        for i, row in enumerate(data):
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = Coffee()
     ex.show()
     sys.exit(app.exec())
